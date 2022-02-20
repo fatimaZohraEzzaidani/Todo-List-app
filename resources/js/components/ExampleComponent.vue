c86b36e79d0fff816485b80c47a577d852186c24
unknown <silasudofia469@gmail.com>
2022-02-19 18:35:26 -0800

    Added Vuejs to the project

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card mt-4">
+                    <div class="card-header">Todo List Component</div>
+
+                    <div class="card-body">
+                        Whats in for today Fatizohra ?
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
